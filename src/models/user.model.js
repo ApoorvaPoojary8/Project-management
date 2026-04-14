f0f73a25004d855f8d3784cd7ea4c795e6844281
ApoorvaPoojary8 <apoorvapoojary8@gmail.com>
2026-04-14 15:40:59 +0530

    implement bcrypt hashing and password comparison

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -61,8 +61,22 @@ const userSchema = new Schema(
             emailVerificationExpiry:{
                 type:Date
             }
+    },{
+        timestamps:true,
     }
+
 )
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
 
 
 
